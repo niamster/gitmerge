5140831cee951c5dbbe441844a72481140acc027
Dmytro Milinevskyy <milinevskyy@gmail.com>
2015-07-07 03:35:45 +0200

    spec prroper ERROR_INFO from simplecov initialization

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
-$ERROR_INFO = 1
+$ERROR_INFO = nil
 
 require 'rubygems'
 require 'bundler/setup'
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear_merged!
-SimpleCov.merge_timeout 3600
-SimpleCov.command_name 'spec'
 
+Coveralls.wear!
 SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 
 require 'gitmerge'
+
 Dir['./spec/support/*.rb'].map { |f| require f }
 
 RSpec.configure(&:disable_monkey_patching!)
